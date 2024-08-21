5da479125bda4cafdfd5cc2b22af571b0db24229
sonn <168544602+sonncoachella@users.noreply.github.com>
2024-08-21 20:47:04 -0300

    consertar o erro das divs

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const avança = document.querySelectorAll('btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
